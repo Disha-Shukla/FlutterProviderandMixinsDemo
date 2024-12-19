cfb79f988822b8a5d43d6a95d7c3de08d8eaa552
Disha A. Shukla <dishashukla463@gmail.com>
2024-12-19 18:39:20 +0530

    Update weather_api_service.dart

--- lib/services/weather_api_service.dart
+++ lib/services/weather_api_service.dart
@@ -5,7 +5,7 @@ class WeatherApiService {
   final Dio _dio = Dio();
 
   Future<WeatherModel> fetchWeather(String city) async {
-    const apiKey = '09c8325eb3a554d7418ad3b835b9ae23';
+    const apiKey = 'APIKey';
     const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
     final response = await _dio.get(baseUrl,
